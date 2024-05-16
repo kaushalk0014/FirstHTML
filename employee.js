711f44e196f27b35b2bc6e3e18ea49fc430aa3f1
kaushal Kumar <151421895+kaushalk0014@users.noreply.github.com>
2024-05-16 21:21:44 +0530

    added modal popup

--- employee.js
+++ employee.js
@@ -1,34 +1,51 @@
- 
- 
- function submitForm(){
-    let employee={}; 
-    let name=document.getElementById("name").value;
-    let address=document.getElementById("address").value;
-    let age=document.getElementById("age").value;
-     
-    
-    if(name==null||name===""){
+
+
+function submitForm() {
+    let employee = {};
+    let name = document.getElementById("name").value;
+    let address = document.getElementById("address").value;
+    let age = document.getElementById("age").value;
+
+
+    if (name == null || name === "") {
         alert("Please enter name");
         return;
     }
-    if(age==null||age===""){
+    if (age == null || age === "") {
         alert("Please select age");
         return;
     }
-    if(address==null||address===""){
+    if (address == null || address === "") {
         alert("Please enter address");
         return;
     }
-   
-    employee.name=name;
-    employee.address=address;
-    employee.age=age;
-    console.log(employee    )
+
+    employee.name = name;
+    employee.address = address;
+    employee.age = age;
+    console.log(employee)
+    
+    $("#submitConfirm").modal('toggle');
     alert(`Ready to submit form ${JSON.stringify(employee)}`);
+   
 }
 
- function resetForm(){
-    document.getElementById("name").value="";
-    document.getElementById("address").value="";
-    document.getElementById("age").value="";
- }
+function submitFormConfirm(){
+    $("#submitConfirm").modal('toggle');
+}
+function resetFormConfirm() {
+    let name = document.getElementById("name").value;
+    let address = document.getElementById("address").value;
+    let age = document.getElementById("age").value;
+    if((name!==null&&name!=="")||(address!==null&&address!=="")||(age!==null&&age!=="")){
+        $("#resetConfirm").modal('toggle');
+    }
+
+    
+}
+function resetForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("age").value = "";
+    $("#resetConfirm").modal('toggle');
+}
