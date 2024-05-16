8e86d372473b1f014159c84143f08ad1aaaf38be
Raushan chauhan <saty.k.0021@gmail.com>
2024-05-16 23:09:24 +0530

    chage code
    create registration page

--- employee.js
+++ employee.js
@@ -3,12 +3,21 @@
 function submitForm() {
     let employee = {};
     let name = document.getElementById("name").value;
-    let address = document.getElementById("address").value;
     let age = document.getElementById("age").value;
+    let address = document.getElementById("address").value;
+    let mobile = document.getElementById("mobile").value;
+    let gender= document.getElementById("gender").value;
+    let date= document.getElementById("date").value;
+    let media= document.getElementById("media").value;
 
     employee.name = name;
-    employee.address = address;
     employee.age = age;
+    employee.address = address;
+    employee.mobile = mobile;
+    employee.gender= gender;
+    employee.date = date;
+    employee.media = media;
+
     console.log(employee)
     
     $("#submitConfirm").modal('toggle');
@@ -18,8 +27,14 @@ function submitForm() {
 
 function submitFormConfirm(){
     let name = document.getElementById("name").value;
-    let address = document.getElementById("address").value;
     let age = document.getElementById("age").value;
+    let address = document.getElementById("address").value;
+    let mobile = document.getElementById("mobile").value;
+    let gender = document.getElementById("gender").value;
+    let date = document.getElementById("date").value;
+    let media = document.getElementById("media").value;
+    
+
 
 
     if (name == null || name === "") {
@@ -34,22 +49,48 @@ function submitFormConfirm(){
         alert("Please enter address");
         return;
     }
+    if (mobile == null || mobile === "") {
+        alert("Please enter mobile");
+        return;
+    }
+    if (gender == null || gender === "") {
+        alert("Please select gender");
+        return;
+    }
+    if (date == null || date === "") {
+        alert("Please enter DOB");
+        return;
+    }
+    if (media == null || media === "") {
+        alert("Please select media");
+        return;
+    }
+
     $("#submitConfirm").modal('toggle');
 }
 
 function resetFormConfirm() {
     let name = document.getElementById("name").value;
-    let address = document.getElementById("address").value;
     let age = document.getElementById("age").value;
-    if((name!==null&&name!=="")||(address!==null&&address!=="")||(age!==null&&age!=="")){
+    let address = document.getElementById("address").value;
+    let mobile = document.getElementById("moble").value;
+    let gender = document.getElementById("gender").value;
+    let date = document.getElementById("date").value;
+    let media = document.getElementById("media").value;
+    
+    if((name!==null&&name!=="")||(age!==null&&age!=="")||(address!==null&&address!=="")||(mobile!==null&&mobile!=="")||(gender!==null&&gender!=="")||(date!==null&&date!=="")||(media!==null&&media!=="")){
         $("#resetConfirm").modal('toggle');
     }
 
     
 }
 function resetForm() {
-    document.getElementById("name").value = "";
-    document.getElementById("address").value = "";
-    document.getElementById("age").value = "";
+    document.getElementById("name").value="";
+    document.getElementById("age").value="";
+    document.getElementById("address").value="";
+    document.getElementById("mob").value="";
+    document.getElementById("gender").value="";
+    document.getElementById("date").value="";
+    document.getElementById("media").value="";
     $("#resetConfirm").modal('toggle');
 }
